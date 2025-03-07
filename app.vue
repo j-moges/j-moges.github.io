5e3d6b2f990040fcf9863e745ab9c51550b2e429
Michael Althoff <lotrunner.help@gmail.com>
2025-03-06 20:13:13 -0500

    seo/head additions

--- app.vue
+++ app.vue
@@ -6,3 +6,17 @@
     <Footer></Footer>
   </div>
 </template>
+
+<script setup>
+  useHead({
+    title: 'Software Engineering Résumé Review',
+    meta: [
+      { name: 'description', content: 'Software Engineering Résumé Review and Job Search Strategy Consulting'},
+      { name: 'author', content: 'Michael Althoff' },
+      { name: 'keywords', content: 'resume, résumé, resume review, software engineering, software developer, programmer, job search strategy, job search consulting, ' },
+      // { name: '', content: '' },
+
+    ]
+
+  })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Michael Althoff Consulting', //default fallback title
+      htmlAttrs: {
+        lang: 'en'
+      },
+    },
+    header: {
+      link: [{rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico'}]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui']
