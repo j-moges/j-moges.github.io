2b1f3b338bdcf6de5796393a8497ccde08509607
Michael Althoff <lotrunner.help@gmail.com>
2025-03-06 21:33:47 -0500

    wording updates and added icon package

--- components/Interview.vue
+++ components/Interview.vue
@@ -19,17 +19,15 @@
                         <div class="-top-[58px] absolute flex inset-x-0 justify-center w-full">
                         </div>
                         <h1 class="text-2xl mb-10">Trouble getting calls back after applying to open positions?</h1>
-                        <p class="m-6">I help experienced software developers and new grads in the software development 
-                            industry get more interviews quicker.
+                        <p class="m-6">I help software developers of any experience level industry land interviews fast.
                         </p>
-                        <p class="m-6">Software engineer, software developer, programmer. Whatever you call yourself, I
-                            can help you market yourself more effectively.</p>
+                        <p class="m-6">Software engineer, software developer, programmer, data scientist. 
+                            Whatever you call yourself, I can help you market yourself more effectively.</p>
                         <p class="m-6">
                             By having an in-depth conversation about your skills, experience, where you want to go,
                             and where you've been I
                             provide actionable steps to
                             accelerate your résumé and job search strategy.
-                            <!-- alt + 0233 = é -->
                         </p>
                         <div>
                             <h1 class="font-bold text-2xl">Why me?</h1>
@@ -57,7 +55,7 @@
                         <div class="-top-[58px] absolute flex inset-x-0 justify-center w-full">
                         </div>
                         <div class="flex flex-col gap-y-4 items-center pb-6 pt-[58px] px-4 rounded-xl sm:px-28 z-10">
-                            <span class="text-md font-semibold">Don't take my word for it. Hear from other people I've
+                            <span class="text-md font-semibold">Hear from other people I've
                                 helped!</span>
                             <TestimonialCarosel />
                         </div>
@@ -89,9 +87,6 @@
                         </ul>
                     </div>
                 </div>
-
-
-
                 <div class="gap-6 grid grid-cols-2 lg:grid-cols-10 max-w-[960px] px-4">
                     <div class="col-span-2 lg:col-span-10 relative items-center ">
                         <div class="absolute inset-y-0 left-0 rounded-xl to-transparent transition-opacity w-[20%] z-1">
@@ -105,25 +100,20 @@
                         </div>
                         <div class="flex flex-col gap-y-4 items-center pb-6 pt-[58px] px-4 rounded-xl sm:px-28 z-10">
                         </div>
-                        <h1 class="text-lg font-semibold">Schedule a call!</h1>
-                        <div>Calendly Link to schedule call then Paypal
-                            <p>do i need to make this much longer before stuff will fit over to the lefft becuase its a flex?</p>
-                        <!-- TODO: get a domain, see about domain email, find a hosting site/platform (domain email here?  who provides?) 
-                                    sign up for calendly (free), create a business paypal account (create new checking account?),
-                                    use google meet for conferencing and integrate with calendly, setup paypal-calendly integration,
-                                    add calendly/paypal to the website and test
-
-                                    Add SEO stuff through Nuxt for the page to start driving traffic
-                        -->
-                        
-                        </div>
+                        <h1 class="text-lg font-semibold">
+                            <!-- Schedule a call! -->
+                            <ULink to="https://calendly.com/d-michaelalthoff/60min"
+                                active-class="text-primary"
+                                class="text-blue-600 dark:text-blue-500 hover:underline"
+                                >
+                                <Icon name="logos:google-calendar"/>&nbsp;Click Here to Schedule A Call!
+                            </ULink>
+                        </h1>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-
 </template>
 
 
@@ -131,5 +121,4 @@
 import TestimonialCarosel from './TestimonialCarosel.vue';
 </script>
 <style>
-/* @import url("~/assets/css/index.css"); */
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,5 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui', '@nuxt/icon']
 })
